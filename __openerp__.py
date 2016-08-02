3b5f167a349984170e3d5608933b1324290a9c89
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-02 16:34:40 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Accounting - Invoice tags',
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['base','account'],
+    'data': [
+	'account_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
---
+++ models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from openerp import models, api, fields, exceptions
+from openerp.exceptions import ValidationError
+from datetime import date
+
+class account_invoice(models.Model):
+	_inherit = "account.invoice"
+
+	customer_tags = fields.Many2many(comodel_name='res.partner.category',relation='invoice_category',column1='partner_id',column2='invoice_id')
+
+	@api.model
+	def _update_customer_tags(self):
+		invoices = self.search([('customer_tags','=',False)])
+		for invoice in invoices:
+			if invoice.partner_id.category_id:
+				vals = {
+					'customer_tags': invoice.partner_id.category_id
+					}
+				invoice.write(vals)
+		return None
